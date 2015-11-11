b4b414513be0966c37102878f8d685cf9c8e850b
Nahid <nahidaslani.1988@gmail.com>
2015-11-11 22:20:37 +0330

    First init core of joomla v3

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-25 22:23:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-10-25T22:23:41+00:00	INFO	joomlafailure	نام کاربری یا کلمه عبور صحیح نیستند و یا شما شناسه ای در سایت ندارید
+2015-10-25T22:23:52+00:00	INFO	joomlafailure	نام کاربری یا کلمه عبور صحیح نیستند و یا شما شناسه ای در سایت ندارید
+2015-10-25T22:24:03+00:00	INFO	joomlafailure	کاربر موجود نیست
+2015-10-25T22:24:15+00:00	INFO	joomlafailure	نام کاربری یا کلمه عبور صحیح نیستند و یا شما شناسه ای در سایت ندارید
---
+++ templates/protostar/index.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Getting params from template
+$params = JFactory::getApplication()->getTemplate(true)->params;
+
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$this->language = $doc->language;
+$this->direction = $doc->direction;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->getCfg('sitename');
+
+if($task == "edit" || $layout == "form" )
+{
+	$fullWidth = 1;
+}
+else
+{
+	$fullWidth = 0;
+}
+
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+$doc->addScript('templates/' .$this->template. '/js/template.js');
+
+// Add Stylesheets
+$doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
+
+// Load optional RTL Bootstrap CSS
+JHtml::_('bootstrap.loadCss', false, $this->direction);
+
+// Add current user information
+$user = JFactory::getUser();
+
+// Adjusting content width
+if ($this->countModules('position-7') && $this->countModules('position-8'))
+{
+	$span = "span6";
+}
+elseif ($this->countModules('position-7') && !$this->countModules('position-8'))
+{
+	$span = "span9";
+}
+elseif (!$this->countModules('position-7') && $this->countModules('position-8'))
+{
+	$span = "span9";
+}
+else
+{
+	$span = "span12";
+}
+
+// Logo file or site title param
+if ($this->params->get('logoFile'))
+{
+	$logo = '<img src="'. JUri::root() . $this->params->get('logoFile') .'" alt="'. $sitename .'" />';
+}
+elseif ($this->params->get('sitetitle'))
+{
+	$logo = '<span class="site-title" title="'. $sitename .'">'. htmlspecialchars($this->params->get('sitetitle')) .'</span>';
+}
+else
+{
+	$logo = '<span class="site-title" title="'. $sitename .'">'. $sitename .'</span>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+	<?php
+	// Use of Google Font
+	if ($this->params->get('googleFont'))
+	{
+	?>
+		<link href='//fonts.googleapis.com/css?family=<?php echo $this->params->get('googleFontName');?>' rel='stylesheet' type='text/css' />
+		<style type="text/css">
+			h1,h2,h3,h4,h5,h6,.site-title{
+				font-family: '<?php echo str_replace('+', ' ', $this->params->get('googleFontName'));?>', sans-serif;
+			}
+		</style>
+	<?php
+	}
+	?>
+	<?php
+	// Template color
+	if ($this->params->get('templateColor'))
+	{
+	?>
+	<style type="text/css">
+		body.site
+		{
+			border-top: 3px solid <?php echo $this->params->get('templateColor');?>;
+			background-color: <?php echo $this->params->get('templateBackgroundColor');?>
+		}
+		a
+		{
+			color: <?php echo $this->params->get('templateColor');?>;
+		}
+		.navbar-inner, .nav-list > .active > a, .nav-list > .active > a:hover, .dropdown-menu li > a:hover, .dropdown-menu .active > a, .dropdown-menu .active > a:hover, .nav-pills > .active > a, .nav-pills > .active > a:hover,
+		.btn-primary
+		{
+			background: <?php echo $this->params->get('templateColor');?>;
+		}
+		.navbar-inner
+		{
+			-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
+			-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
+			box-shadow: 0 1px 3px rgba(0, 0, 0, .25), inset 0 -1px 0 rgba(0, 0, 0, .1), inset 0 30px 10px rgba(0, 0, 0, .2);
+		}
+	</style>
+	<?php
+	}
+	?>
+	<!--[if lt IE 9]>
+		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body class="site <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+	. ($params->get('fluidContainer') ? ' fluid' : '');
+?>">
+
+	<!-- Body -->
+	<div class="body">
+		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
+			<!-- Header -->
+			<header class="header" role="banner">
+				<div class="header-inner clearfix">
+					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
+						<?php echo $logo;?> <?php if ($this->params->get('sitedescription')) { echo '<div class="site-description">'. htmlspecialchars($this->params->get('sitedescription')) .'</div>'; } ?>
+					</a>
+					<div class="header-search pull-right">
+						<jdoc:include type="modules" name="position-0" style="none" />
+
+						<!-- Social Nets -->
+						<div class="social" >
+							<p>
+								<?php if($this->params->get('facebook', 'http://www.facebook.com/joomlafarsi')): ?>
+								<a href="<?php echo $this->params->get('facebook', 'http://www.facebook.com/joomlafarsi'); ?>" target="_blank" class="hasTooltip" data-placement="bottom" data-original-title="فیس بوک" >
+									<img src="images/social/fb.png" width="30" height="30" />
+								</a> 
+								<?php endif; ?>
+								
+								<?php if($this->params->get('twitter', 'http://www.twitter.com/#!/mambolearn')): ?>
+								<a href="<?php echo $this->params->get('twitter', 'http://www.twitter.com/#!/mambolearn'); ?>" target="_blank" class="hasTooltip" data-placement="bottom" data-original-title="توئیتر">
+									<img src="images/social/tw.png" alt="twitter" width="30" height="30" />
+								</a> 
+								<?php endif; ?>
+
+								<?php if($this->params->get('gplus', 'https://plus.google.com/u/0/103568652372399562868/posts')): ?>
+								<a href="<?php echo $this->params->get('gplus', 'https://plus.google.com/u/0/103568652372399562868/posts'); ?>" target="_blank" class="hasTooltip" data-placement="bottom" data-original-title="گوگل پلاس">
+									<img src="images/social/gp.png" alt="Google Plus" width="30" height="30" />
+								</a> 
+								<?php endif; ?>
+
+								<?php if($this->params->get('rss', 'http://www.joomlafarsi.com/rss.feed')): ?>
+								<a href="<?php echo $this->params->get('rss', 'http://www.joomlafarsi.com/rss.feed'); ?>" target="_blank" class="hasTooltip" data-placement="bottom" data-original-title="فید RSS">
+									<img src="images/social/rss.png" alt="RSS" width="30" height="30" />
+								</a>
+								<?php endif; ?>
+								
+							</p>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</header>
+			<?php if ($this->countModules('position-1')) : ?>
+			<nav class="navigation" role="navigation">
+				<jdoc:include type="modules" name="position-1" style="none" />
+			</nav>
+			<?php endif; ?>
+			<jdoc:include type="modules" name="banner" style="xhtml" />
+			<div class="row-fluid">
+				<?php if ($this->countModules('position-8')) : ?>
+				<!-- Begin Sidebar -->
+				<div id="sidebar" class="span3">
+					<div class="sidebar-nav">
+						<jdoc:include type="modules" name="position-8" style="xhtml" />
+					</div>
+				</div>
+				<!-- End Sidebar -->
+				<?php endif; ?>
+				<main id="content" role="main" class="<?php echo $span;?>">
+					<!-- Begin Content -->
+					<jdoc:include type="modules" name="position-3" style="xhtml" />
+					<jdoc:include type="message" />
+					<jdoc:include type="component" />
+					<jdoc:include type="modules" name="position-2" style="none" />
+					<!-- End Content -->
+				</main>
+				<?php if ($this->countModules('position-7')) : ?>
+				<div id="aside" class="span3">
+					<!-- Begin Right Sidebar -->
+					<jdoc:include type="modules" name="position-7" style="well" />
+					<!-- End Right Sidebar -->
+				</div>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	<!-- Footer -->
+	<footer class="footer" role="contentinfo">
+		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
+			<hr />
+			<jdoc:include type="modules" name="footer" style="none" />
+			<p class="pull-right">
+				<a href="#top" id="back-top">
+					<?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?>
+				</a>
+			</p>
+			<p>
+   				<?php echo date('Y');?> &copy; <?php echo $sitename; ?>
+			</p>
+		</div>
+	</footer>
+	<jdoc:include type="modules" name="debug" style="none" />
+	<?php if($this->params->get('gplus-box', 'http://forum.joomlafarsi.com')): ?>
+	<div style="border:1px solid #2a668e; z-index: 500;color:#333;font-size:11px;line-height:20px;padding:5px;border-top-right-radius:8px;border-top-left-radius:8px;text-align:center;font-size:8pt;position:fixed;right:0px;bottom:10px;background-color:#FFE5F7;background-repeat:repeat; font-family:tahoma" dir="rtl">
+برای حمایت از ما امتیاز دهید<br /><script src="http://apis.google.com/js/plusone.js" type="text/javascript" gapi_processed="true">
+{lang: 'fa'}</script><g:plusone href="http://forum.joomlafarsi.com"></g:plusone></div>
+	<?php endif; ?>
+</body>
+</html>
